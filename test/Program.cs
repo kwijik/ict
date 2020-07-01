2c2639ea4169aaacdf17988c85cf3bc18bbe318b
kwijik <joebalalaika@ya.ru>
2020-07-01 12:59:04 +0200

    Initial commit

---
+++ test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TestML.Model;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ModelBuilder.CreateModel();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
